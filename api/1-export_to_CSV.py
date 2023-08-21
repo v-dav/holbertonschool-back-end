0b07afbf840f1420d91bcbed0ad58d005fa5a20a
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-08-21 04:47:10 -0700

    write Python script to export data in the CSV format

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""A Python script that, using REST API, for a given employee ID,
+returns information about his/her TODO list progress"""
+
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    """GET response from URL"""
+    todos_response = requests.get("https://jsonplaceholder.typicode.com/todos")
+    employees_response = requests.get(
+        "https://jsonplaceholder.typicode.com/users")
+
+    """Convert response to json file"""
+    todos = todos_response.json()
+    employees = employees_response.json()
+
+    """Requierements"""
+    employee_id = int(sys.argv[1])
+
+    for employee in employees:
+        if employee.get("id") == employee_id:
+            our_employee = employee
+
+    filename = str(our_employee.get("id")) + ".csv"
+
+    with open(filename, 'w', newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            if task.get("userId") == employee_id:
+                writer.writerow([our_employee.get("id"),
+                                our_employee.get("username"),
+                                task.get("completed"),
+                                task.get("title")])
